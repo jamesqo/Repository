8327b4f0e069344fdc150e89aa9f8135a776569b
James Ko <jamesqko@gmail.com>
2017-06-07 20:21:06 -0400

    [Initial commit] Get first few pages working

---
+++ Repository/ChooseProviderActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Repository.Internal;
+
+namespace Repository
+{
+    [Activity(Label = "Choose a Provider")]
+    public class ChooseProviderActivity : Activity
+    {
+        private Button _githubButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ChooseProvider);
+
+            _githubButton = FindViewById<Button>(Resource.Id.GitHubButton);
+            _githubButton.Click += GitHubButton_Click;
+        }
+
+        private void GitHubButton_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(SignInActivity));
+            intent.PutExtra(Strings.SignIn_Url, )
+            StartActivity(intent);
+        }
+    }
+}
---
+++ Repository/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace Repository
+{
+    [Activity(Label = "Repository", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button _getStartedButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            _getStartedButton = FindViewById<Button>(Resource.Id.GetStartedButton);
+            _getStartedButton.Click += GetStartedButton_Click;
+        }
+
+        private void GetStartedButton_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(ChooseProviderActivity));
+            StartActivity(intent);
+        }
+    }
+}
+
