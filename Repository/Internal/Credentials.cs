9e18e9a3eb894822deaf72111ec39ab458482b62
James Ko <jamesqko@gmail.com>
2017-06-07 20:34:17 -0400

    Add Credentials class

---
+++ Repository/Internal/Credentials.cs
@@ -0,0 +1,9 @@
+﻿namespace Repository.Internal
+{
+    internal static class Credentials
+    {
+        public static string ClientId { get; } = "YOUR_CLIENT_ID";
+
+        public static string ClientSecret { get; } = "YOUR_CLIENT_SECRET";
+    }
+}
