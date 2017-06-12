43a72b473d9a2b67e656876cf5d81aae8f05ccfa
James Ko <jamesqko@gmail.com>
2017-06-11 23:23:13 -0400

    [SyntaxHighlighting] Make the parse tree-based highlighter work. :tada:

--- Repository/Internal/EditorServices/SyntaxHighlighting/JavaSyntaxHighlighter.cs
+++ Repository/Internal/EditorServices/SyntaxHighlighting/JavaSyntaxHighlighter.cs
@@ -22,7 +22,7 @@ namespace Repository.Internal.EditorServices.SyntaxHighlighting
 
             private int _index;
             private int _lastTokenIndex;
-            private SyntaxKind _defaultKind;
+            private SyntaxKind _kindOverride;
 
             internal Visitor(string text, ISyntaxStyler styler)
             {
@@ -31,29 +31,10 @@ namespace Repository.Internal.EditorServices.SyntaxHighlighting
                 _styler = styler;
                 _stream = AntlrUtilities.TokenStream(text, input => new JavaLexer(input));
                 _lastTokenIndex = -1;
-                _defaultKind = SyntaxKind.None;
+                _kindOverride = SyntaxKind.None;
             }
 
-            public override object VisitAnnotation([NotNull] AnnotationContext context)
-            {
-                var defaultKind = _defaultKind;
-                int childCount = context.ChildCount;
-                for (int i = 0; i < childCount; i++)
-                {
-                    var child = context.GetChild(i);
-                    switch (child)
-                    {
-                        case ITerminalNode node:
-                            Advance(node, defaultKind);
-                            break;
-                        default:
-                            Visit(child);
-                            break;
-                    }
-                }
-
-                return null;
-            }
+            public override object VisitAnnotation([NotNull] AnnotationContext context) => VisitKeepKindOverride(context);
 
             public override object VisitAnnotationName([NotNull] AnnotationNameContext context)
             {
@@ -64,7 +45,7 @@ namespace Repository.Internal.EditorServices.SyntaxHighlighting
                     switch (child)
                     {
                         case QualifiedNameContext qualifiedName:
-                            _defaultKind = SyntaxKind.Annotation;
+                            _kindOverride = SyntaxKind.Annotation;
                             VisitQualifiedName(qualifiedName);
                             break;
                         default:
@@ -76,93 +57,17 @@ namespace Repository.Internal.EditorServices.SyntaxHighlighting
                 return null;
             }
 
-            public override object VisitClassDeclaration([NotNull] ClassDeclarationContext context)
-            {
-                var defaultKind = _defaultKind;
-                int childCount = context.ChildCount;
-                for (int i = 0; i < childCount; i++)
-                {
-                    var child = context.GetChild(i);
-                    switch (child)
-                    {
-                        case ITerminalNode node:
-                            Advance(node, defaultKind);
-                            break;
-                        default:
-                            Visit(child);
-                            break;
-                    }
-                }
+            public override object VisitClassDeclaration([NotNull] ClassDeclarationContext context) => VisitKeepKindOverride(context);
 
-                return null;
-            }
+            public override object VisitClassOrInterfaceModifier([NotNull] ClassOrInterfaceModifierContext context) => VisitKeepKindOverride(context);
 
-            public override object VisitClassOrInterfaceModifier([NotNull] ClassOrInterfaceModifierContext context)
-            {
-                var defaultKind = _defaultKind;
-                int childCount = context.ChildCount;
-                for (int i = 0; i < childCount; i++)
-                {
-                    var child = context.GetChild(i);
-                    switch (child)
-                    {
-                        case ITerminalNode node:
-                            Advance(node, defaultKind);
-                            break;
-                        default:
-                            Visit(child);
-                            break;
-                    }
-                }
+            public override object VisitPackageDeclaration([NotNull] PackageDeclarationContext context) => VisitKeepKindOverride(context);
 
-                return null;
-            }
-
-            public override object VisitPackageDeclaration([NotNull] PackageDeclarationContext context)
-            {
-                var defaultKind = _defaultKind;
-                int childCount = context.ChildCount;
-                for (int i = 0; i < childCount; i++)
-                {
-                    var child = context.GetChild(i);
-                    switch (child)
-                    {
-                        case ITerminalNode node:
-                            Advance(node, defaultKind);
-                            break;
-                        default:
-                            Visit(child);
-                            break;
-                    }
-                }
-
-                return null;
-            }
-
-            public override object VisitImportDeclaration([NotNull] ImportDeclarationContext context)
-            {
-                var defaultKind = _defaultKind;
-                int childCount = context.ChildCount;
-                for (int i = 0; i < childCount; i++)
-                {
-                    var child = context.GetChild(i);
-                    switch (child)
-                    {
-                        case ITerminalNode node:
-                            Advance(node, defaultKind);
-                            break;
-                        default:
-                            Visit(child);
-                            break;
-                    }
-                }
-
-                return null;
-            }
+            public override object VisitImportDeclaration([NotNull] ImportDeclarationContext context) => VisitKeepKindOverride(context);
 
             public override object VisitTerminal(ITerminalNode node)
             {
-                Advance(node, _defaultKind);
+                Advance(node, _kindOverride);
                 return null;
             }
 
@@ -178,10 +83,10 @@ namespace Repository.Internal.EditorServices.SyntaxHighlighting
                 Surpass(token, kind);
             }
 
-            private void Advance(ITerminalNode node, SyntaxKind defaultKind)
+            private void Advance(ITerminalNode node, SyntaxKind kindOverride)
             {
                 var token = node.Symbol;
-                var kind = SuggestKind(token).TryOverride(defaultKind);
+                var kind = SuggestKind(token).TryOverride(kindOverride);
                 Advance(token, kind);
             }
 
@@ -219,15 +124,134 @@ namespace Repository.Internal.EditorServices.SyntaxHighlighting
 
                 switch (token.Type)
                 {
-                    case ABSTRACT: return SyntaxKind.Keyword;
-                    // TODO and all.
+                    case AntlrConstants.Eof:
+                        return SyntaxKind.Eof;
+                    case ABSTRACT:
+                    case ASSERT:
+                    case BOOLEAN:
+                    case BREAK:
+                    case BYTE:
+                    case CASE:
+                    case CATCH:
+                    case CHAR:
+                    case CLASS:
+                    case CONST:
+                    case CONTINUE:
+                    case DEFAULT:
+                    case DO:
+                    case DOUBLE:
+                    case ELSE:
+                    case ENUM:
+                    case EXTENDS:
+                    case FINAL:
+                    case FINALLY:
+                    case FLOAT:
+                    case FOR:
+                    case IF:
+                    case GOTO:
+                    case IMPLEMENTS:
+                    case IMPORT:
+                    case INSTANCEOF:
+                    case INT:
+                    case INTERFACE:
+                    case LONG:
+                    case NATIVE:
+                    case NEW:
+                    case PACKAGE:
+                    case PRIVATE:
+                    case PROTECTED:
+                    case PUBLIC:
+                    case RETURN:
+                    case SHORT:
+                    case STATIC:
+                    case STRICTFP:
+                    case SUPER:
+                    case SWITCH:
+                    case SYNCHRONIZED:
+                    case THIS:
+                    case THROW:
+                    case THROWS:
+                    case TRANSIENT:
+                    case TRY:
+                    case VOID:
+                    case VOLATILE:
+                    case WHILE:
+                        return SyntaxKind.Keyword;
+                    case IntegerLiteral:
+                    case FloatingPointLiteral:
+                        return SyntaxKind.NumericLiteral;
+                    case BooleanLiteral:
+                        return SyntaxKind.BooleanLiteral;
+                    case CharacterLiteral:
+                    case StringLiteral:
+                        return SyntaxKind.StringLiteral;
+                    case NullLiteral:
+                        return SyntaxKind.NullLiteral;
+                    case LPAREN:
+                    case RPAREN:
+                    case LBRACE:
+                    case RBRACE:
+                    case LBRACK:
+                    case RBRACK:
+                    case SEMI:
+                    case COMMA:
+                    case DOT:
+                    case ASSIGN:
+                    case GT:
+                    case LT:
+                    case BANG:
+                    case TILDE:
+                    case QUESTION:
+                    case COLON:
+                    case EQUAL:
+                    case LE:
+                    case GE:
+                    case NOTEQUAL:
+                    case AND:
+                    case OR:
+                    case INC:
+                    case DEC:
+                    case ADD:
+                    case SUB:
+                    case MUL:
+                    case DIV:
+                    case BITAND:
+                    case BITOR:
+                    case CARET:
+                    case MOD:
+                    case ADD_ASSIGN:
+                    case SUB_ASSIGN:
+                    case MUL_ASSIGN:
+                    case DIV_ASSIGN:
+                    case AND_ASSIGN:
+                    case OR_ASSIGN:
+                    case XOR_ASSIGN:
+                    case MOD_ASSIGN:
+                    case LSHIFT_ASSIGN:
+                    case RSHIFT_ASSIGN:
+                    case URSHIFT_ASSIGN:
+                        return SyntaxKind.Identifier;
+                    case Identifier:
+                        return SyntaxKindSuggestion.Overridable(SyntaxKind.Identifier);
+                    case AT:
+                        return SyntaxKind.Annotation;
+                    case ELLIPSIS:
+                        return SyntaxKind.Identifier;
+                    default:
+                        throw new NotSupportedException();
                 }
             }
 
             private void Surpass(IToken token, SyntaxKind kind)
             {
+                Debug.Assert(_lastTokenIndex + 1 == token.TokenIndex);
+
+                if (kind == SyntaxKind.Eof)
+                {
+                    return;
+                }
+
                 _lastTokenIndex++;
-                Debug.Assert(_lastTokenIndex == token.TokenIndex);
 
                 int count = token.Text.Length;
                 var span = _styler.GetSpan(kind);
@@ -239,6 +263,41 @@ namespace Repository.Internal.EditorServices.SyntaxHighlighting
             {
                 var kind = GetHiddenKind(token);
                 Surpass(token, kind);
+            }
+
+            private object VisitKeepKindOverride(ParserRuleContext context, bool mayHaveTerminalChildren = true)
+            {
+                var kindOverride = _kindOverride;
+
+                bool TryHandleTerminal(IParseTree child)
+                {
+                    if (mayHaveTerminalChildren)
+                    {
+                        if (child is ITerminalNode node)
+                        {
+                            Advance(node, kindOverride);
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        Debug.Assert(!(child is ITerminalNode));
+                    }
+
+                    return false;
+                }
+
+                int childCount = context.ChildCount;
+                for (int i = 0; i < childCount; i++)
+                {
+                    var child = context.GetChild(i);
+                    if (!TryHandleTerminal(child))
+                    {
+                        Visit(child);
+                    }
+                }
+
+                return null;
             }
         }
 
