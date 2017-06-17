daa39785c2e446184d9d576038b47fcd4c2d83a1
James Ko <jamesqko@gmail.com>
2017-06-17 03:32:46 -0400

    [WIP] SpannableText to avoid Android's quadratic behavior of SetSpan() with lots of spans

---
+++ Repository/Internal/EditorServices/Highlighting/SpannableText.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Android.Runtime;
+using Android.Text;
+using Java.Lang;
+using Repository.Common;
+
+namespace Repository.Internal.EditorServices.Highlighting
+{
+    internal class SpannableText : Java.Lang.Object, ISpannable
+    {
+        private struct SpanInfo
+        {
+            public SpanInfo(int start, int end, SpanTypes flags)
+            {
+                Start = start;
+                End = end;
+                Flags = flags;
+            }
+
+            public int End { get; }
+
+            public SpanTypes Flags { get; }
+
+            public int Start { get; }
+        }
+
+        private readonly string _rawText;
+        private readonly TwoWayDictionary<Java.Lang.Object, SpanInfo> _map;
+        private readonly List<SpanInfo> _infos;
+
+        private SpannableText(string rawText)
+        {
+            Verify.NotNull(rawText, nameof(rawText));
+
+            _rawText = rawText;
+            _map = new TwoWayDictionary<Java.Lang.Object, SpanInfo>(
+                // Avoid calls to Java.Lang.Object.GetHashCode(), Java marshalling is slow.
+                forwardsComparer: ReferenceEqualityComparer.Instance);
+            _infos = new List<SpanInfo>();
+        }
+
+        public char CharAt(int index) => _rawText[index];
+
+        public IEnumerator<char> GetEnumerator() => _rawText.GetEnumerator();
+
+        public int GetSpanEnd(Java.Lang.Object tag)
+            => _map.TryGetForwards(tag, out var info) ? info.End : -1;
+
+        [return: GeneratedEnum]
+        public SpanTypes GetSpanFlags(Java.Lang.Object tag)
+            => _map.TryGetForwards(tag, out var info) ? info.Flags : default(SpanTypes);
+
+        public Java.Lang.Object[] GetSpans(int start, int end, Class type)
+        {
+            for (int i = 0; i < _infos.Count; i++)
+            {
+                var info = _infos[i];
+
+                int spanStart = info.Start, spanEnd = info.End;
+                if (end < spanStart || spanEnd < start)
+                {
+                    continue;
+                }
+
+                Debug.Assert(spanStart != spanEnd);
+                if (start != end && (end == spanStart || spanEnd == start))
+                {
+                    continue;
+                }
+
+                var span = _map.GetBackwards(info);
+                // TODO: Missed out on optimization for `type` being Object.class.
+                // See https://github.com/android/platform_frameworks_base/blob/b5c4e80ecd47dda8c73b0e93eb2ee1a8da58c981/core/java/android/text/SpannableStringInternal.java#L313
+            }
+        }
+
+        public int GetSpanStart(Java.Lang.Object tag)
+            => _map.TryGetForwards(tag, out var info) ? info.Start : -1;
+
+        public int Length() => _rawText.Length;
+
+        public int NextSpanTransition(int start, int limit, Class type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSpan(Java.Lang.Object what) => throw new NotSupportedException();
+
+        public void SetSpan(Java.Lang.Object what, int start, int end, [GeneratedEnum] SpanTypes flags)
+        {
+            var info = new SpanInfo(start, end, flags);
+            _map.Add(what, info);
+            _infos.Add(info);
+        }
+
+        public ICharSequence SubSequenceFormatted(int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Repository/Internal/TwoWayDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Internal
+{
+    internal class TwoWayDictionary<T1, T2>
+    {
+        private readonly Dictionary<T1, T2> _forwards;
+        private readonly Dictionary<T2, T1> _backwards;
+
+        public TwoWayDictionary(
+            IEqualityComparer<T1> forwardsComparer = null,
+            IEqualityComparer<T2> backwardsComparer = null)
+        {
+            _forwards = new Dictionary<T1, T2>(forwardsComparer);
+            _backwards = new Dictionary<T2, T1>(backwardsComparer);
+        }
+
+        public int Count
+        {
+            get
+            {
+                Debug.Assert(_forwards.Count == _backwards.Count);
+                return _forwards.Count;
+            }
+        }
+
+        public void Add(T1 first, T2 second)
+        {
+            _forwards.Add(first, second);
+            _backwards.Add(second, first);
+        }
+
+        public T1 GetBackwards(T2 key) => _backwards[key];
+
+        public bool TryGetForwards(T1 key, out T2 value) => _forwards.TryGetValue(key, out value);
+    }
+}
