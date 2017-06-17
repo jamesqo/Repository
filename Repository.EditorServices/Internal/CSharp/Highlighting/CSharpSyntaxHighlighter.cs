1a7ed4d0d556bd0b9311c532c5d6685e73590af7
James Ko <jamesqko@gmail.com>
2017-06-16 21:41:50 -0400

    [WIP] Manual C# syntax highlighting

--- Repository.EditorServices/Internal/CSharp/Highlighting/CSharpSyntaxHighlighter.cs
+++ Repository.EditorServices/Internal/CSharp/Highlighting/CSharpSyntaxHighlighter.cs
@@ -2,139 +2,44 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Classification;
-using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp;
 using Repository.EditorServices.Highlighting;
-using static Microsoft.CodeAnalysis.Classification.ClassificationTypeNames;
+using CommonSyntaxKind = Repository.EditorServices.Highlighting.SyntaxKind;
 
 namespace Repository.EditorServices.Internal.CSharp.Highlighting
 {
     internal class CSharpSyntaxHighlighter : ISyntaxHighlighter
     {
-        private class Worker
+        private class Visitor : CSharpSyntaxVisitor
         {
-            private readonly string _sourceText;
+            private readonly SyntaxTree _tree;
             private readonly ISyntaxColorer _colorer;
-            private readonly IEnumerable<ClassifiedSpan> _spans;
 
             private int _index;
 
-            internal Worker(string sourceText, ISyntaxColorer colorer)
+            internal Visitor(string text, ISyntaxColorer colorer)
             {
-                _sourceText = sourceText;
+                _tree = CSharpSyntaxTree.ParseText(text);
                 _colorer = colorer;
-                _spans = GetClassifiedSpans(sourceText);
             }
 
-            internal void Run()
+            public override void DefaultVisit(SyntaxNode node)
             {
-                foreach (var span in _spans)
-                {
-                    var textSpan = span.TextSpan;
-                    HandleSkippedText(_index, textSpan.Start);
-                    Advance(textSpan.Length, GetSyntaxKind(span.ClassificationType));
-                }
+
             }
 
-            private void HandleSkippedText(int lastSpanEnd, int spanStart)
-            {
-                if (lastSpanEnd < spanStart)
-                {
-                    int skipped = spanStart - lastSpanEnd;
-                    Advance(skipped, SyntaxKind.Plaintext);
-                }
-            }
+            internal void Run() => Visit(_tree.GetRoot());
 
-            private void Advance(int count, SyntaxKind kind)
+            private void Advance(int count, CommonSyntaxKind kind)
             {
                 _colorer.Color(kind, _index, count);
                 _index += count;
-            }
-
-            private static Document CreateDocument(string sourceText)
-            {
-                var workspace = new AdhocWorkspace();
-                var solution = workspace.CurrentSolution;
-                var project = solution.AddProject(string.Empty, string.Empty, LanguageNames.CSharp);
-                return project.AddDocument(string.Empty, sourceText);
-            }
-
-            private static IEnumerable<ClassifiedSpan> GetClassifiedSpans(string sourceText)
-            {
-                var document = CreateDocument(sourceText);
-                // TODO: Add async support to ISyntaxHighlighter? IsAsync, or just force everything to return a Task?
-                return Classifier.GetClassifiedSpansAsync(document, new TextSpan(0, sourceText.Length)).Result;
-            }
-
-            private static SyntaxKind GetSyntaxKind(string classificationType)
-            {
-                switch (classificationType)
-                {
-                    case ClassName:
-                        // TODO: The C# APIs don't offer distinction between type declarations and type idents like ANTLR does.
-                        // What should be our behavior here?
-                        return SyntaxKind.TypeDeclaration;
-                    case Comment:
-                        return SyntaxKind.Comment;
-                    case DelegateName:
-                    case EnumName:
-                        return SyntaxKind.TypeDeclaration;
-                    // TODO: Consider this more carefully. What if all #if ...s get excluded?
-                    case ExcludedCode:
-                        // TODO: Implement in ColorTheme
-                        return SyntaxKind.ExcludedCode;
-                    case Identifier:
-                        return SyntaxKind.Identifier;
-                    case InterfaceName:
-                        return SyntaxKind.TypeDeclaration;
-                    case Keyword:
-                        return SyntaxKind.Keyword;
-                    case ModuleName:
-                        // TODO: What is this?
-                        throw new NotSupportedException();
-                    case NumericLiteral:
-                        return SyntaxKind.NumericLiteral;
-                    case Operator:
-                        return SyntaxKind.Operator;
-                    case PreprocessorKeyword:
-                        return SyntaxKind.Keyword;
-                    case PreprocessorText:
-                    case Punctuation:
-                        return SyntaxKind.Plaintext;
-                    case StringLiteral:
-                        return SyntaxKind.StringLiteral;
-                    case StructName:
-                        return SyntaxKind.TypeDeclaration;
-                    case Text:
-                        // TODO: When is this actually hit? Is Plaintext appropriate?
-                        return SyntaxKind.Plaintext;
-                    case TypeParameterName:
-                        // TODO: Is this the best choice? Should we add SyntaxKind.TypeParameterDeclaration?
-                        return SyntaxKind.TypeIdentifier;
-                    case VerbatimStringLiteral:
-                        return SyntaxKind.StringLiteral;
-                    case WhiteSpace:
-                        return SyntaxKind.Plaintext;
-                    case XmlDocCommentAttributeName:
-                    case XmlDocCommentAttributeQuotes:
-                    case XmlDocCommentAttributeValue:
-                    case XmlDocCommentCDataSection:
-                    case XmlDocCommentComment:
-                    case XmlDocCommentDelimiter:
-                    case XmlDocCommentEntityReference:
-                    case XmlDocCommentName:
-                    case XmlDocCommentProcessingInstruction:
-                    case XmlDocCommentText:
-                        return SyntaxKind.Comment;
-                    default:
-                        throw new NotSupportedException();
-                }
             }
         }
 
         public TResult Highlight<TResult>(string text, ISyntaxColorer<TResult> colorer)
         {
-            new Worker(text, colorer).Run();
+            new Visitor(text, colorer).Run();
             return colorer.Result;
         }
     }
