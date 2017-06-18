abfc57128671f56a2e7da0452f70659bb3622b4f
James Ko <jamesqko@gmail.com>
2017-06-18 12:00:11 -0400

    Finish build script for building the AAR

---
+++ rebuild-aar.ps1
@@ -0,0 +1,13 @@
+$aarName = "repository-release.aar"
+$javaProjectRoot = [IO.Path]::Combine($PSScriptRoot, "java")
+$aarSource = [IO.Path]::Combine($javaProjectRoot, "repository", "build", "outputs", "aar", $aarName)
+$javaInteropRoot = [IO.Path]::Combine($PSScriptRoot, "Repository.JavaInterop")
+$aarDestination = [IO.Path]::Combine($javaInteropRoot, "Jars")
+
+pushd $javaProjectRoot
+
+.\gradlew assembleRelease
+cp $aarSource $aarDestination -Force
+echo "Successfully copied $aarName to $aarDestination."
+
+popd
