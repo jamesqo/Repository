081a6d70f7623723f02cfcdadbc5b3ea1c84ba38
James Ko <jamesqko@gmail.com>
2017-06-17 21:38:00 -0400

    TextColorer simply stores up the Colorings for the Java code to process now, doesn't touch spans.

--- Repository/EditorServices/Highlighting/TextColorer.cs
+++ Repository/EditorServices/Highlighting/TextColorer.cs
@@ -1,40 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.Text;
-using Android.Text.Style;
+using System.Diagnostics;
+using Android.Graphics;
 using Repository.Common;
-using Repository.Internal.EditorServices.Highlighting;
+using Repository.Internal;
+using Coloring = System.Int64;
 
 namespace Repository.EditorServices.Highlighting
 {
-    public class TextColorer : ITextColorer<SpannableText>
+    public class TextColorer : ITextColorer<ColoredText>
     {
-        private readonly SpannableText _text;
+        private readonly string _text;
         private readonly IColorTheme _theme;
-
-        private int _index;
+        private readonly ArrayBuilder<Coloring> _colorings;
 
         private TextColorer(string text, IColorTheme theme)
         {
             Verify.NotNull(text, nameof(text));
             Verify.NotNull(theme, nameof(theme));
 
-            _text = SpannableText.Create(text);
+            _text = text;
             _theme = theme;
+            _colorings = new ArrayBuilder<Coloring>();
         }
 
         public static TextColorer Create(string text, IColorTheme theme) => new TextColorer(text, theme);
 
-        public SpannableText Result => _text;
+        public ColoredText Result => new ColoredText(_text, _colorings.Array, _colorings.Count);
 
         public void Color(SyntaxKind kind, int count)
         {
             var color = _theme.GetForegroundColor(kind);
-            var span = new ForegroundColorSpan(color);
-            _text.SetSpan(span, _index, _index + count, SpanTypes.InclusiveExclusive);
-            _index += count;
+            _colorings.Add(MakeColoring(color, count));
+        }
+
+        private static Coloring MakeColoring(Color color, int count)
+        {
+            Debug.Assert(count >= 0);
+            return ((long)color.ToArgb() << 32) | (uint)count;
         }
     }
 }
