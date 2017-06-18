44e08a0b53cbd037259fbdadf050e00d1becb5f3
James Ko <jamesqko@gmail.com>
2017-06-18 12:39:56 -0400

    [Perf] Add FragmentedByteBuffer

---
+++ Repository/Internal/ArrayBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Repository.Common;
+
+namespace Repository.Internal
+{
+    [DebuggerDisplay(DebuggerStrings.DisplayFormat)]
+    internal struct ArrayBuilder<T>
+    {
+        private T[] _array;
+        private int _count;
+
+        public void Add(T item)
+        {
+            if (_count == Capacity)
+            {
+                Resize();
+            }
+
+            _array[_count++] = item;
+        }
+
+        public int Capacity => _array?.Length ?? 0;
+
+        public int Count => _count;
+
+        public T this[int index]
+        {
+            get
+            {
+                Debug.Assert(index < _count);
+                return _array[index];
+            }
+        }
+
+        private string DebuggerDisplay => $"{nameof(Count)}: {Count}";
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(_array, 0, array, arrayIndex, _count);
+        }
+
+        private void Resize()
+        {
+            Debug.Assert(_count == Capacity);
+
+            if (_count == 0)
+            {
+                _array = new T[4];
+                return;
+            }
+
+            int newCapacity = 2 * _count;
+            var newArray = new T[newCapacity];
+            Array.Copy(_array, 0, newArray, 0, _count);
+            _array = newArray;
+        }
+    }
+}
---
+++ Repository/Internal/EditorServices/Highlighting/FragmentedByteBuffer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Android.Runtime;
+using Java.Nio;
+using JavaObject = Java.Lang.Object;
+
+namespace Repository.Internal.EditorServices.Highlighting
+{
+    internal class FragmentedByteBuffer : IDisposable
+    {
+        private const int InitialCapacity = 256;
+
+        private readonly ArrayBuilder<ByteBuffer> _previous;
+
+        private ByteBuffer _current;
+        private int _currentCapacity;
+        private int _index;
+
+        public FragmentedByteBuffer()
+        {
+            Allocate(InitialCapacity);
+        }
+
+        public void Add(long value)
+        {
+            Debug.Assert(IsAligned(sizeof(long)));
+
+            if (_index == _currentCapacity)
+            {
+                Allocate();
+            }
+
+            _current.PutLong(value);
+        }
+
+        // TODO: Override Finalize(), have Dispose(bool)?
+        public void Dispose()
+        {
+            for (int i = 0; i < _previous.Count; i++)
+            {
+                Free(_previous[i]);
+            }
+
+            Free(_current);
+        }
+
+        public ByteBuffer[] GetFragments()
+        {
+            var fragments = new ByteBuffer[_previous.Count + 1];
+            _previous.CopyTo(fragments, 0);
+            fragments[_previous.Count] = _current;
+            return fragments;
+        }
+
+        private void Allocate()
+        {
+            Debug.Assert(_index == _currentCapacity);
+            int nextCapacity = _currentCapacity * 2;
+            Allocate(nextCapacity);
+        }
+
+        private void Allocate(int capacity)
+        {
+            _previous.Add(_current);
+            _currentCapacity = capacity;
+            _index = 0;
+
+            // TODO: Refactor into static method?
+            IntPtr handle = Marshal.AllocHGlobal(capacity);
+            var objectHandle = JNIEnv.NewDirectByteBuffer(handle, capacity);
+            // TODO: Is this necessary? Store the raw IntPtrs directly?
+            _current = JavaObject.GetObject<ByteBuffer>(objectHandle, JniHandleOwnership.TransferLocalRef);
+        }
+
+        private static void Free(ByteBuffer fragment)
+        {
+            IntPtr handle = fragment.GetDirectBufferAddress();
+            Marshal.FreeHGlobal(handle);
+        }
+
+        private bool IsAligned(int byteCount) => _index % byteCount == 0;
+    }
+}
