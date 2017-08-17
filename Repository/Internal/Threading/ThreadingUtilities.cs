98a6eb875f47f7c5959a815e8954a75d21a75d20
James Ko <jamesqko@gmail.com>
2017-08-16 20:18:05 -0400

    Remove TODO

--- Repository/Internal/Threading/ThreadingUtilities.cs
+++ Repository/Internal/Threading/ThreadingUtilities.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.OS;
 
 namespace Repository.Internal.Threading
 {
     internal static class ThreadingUtilities
     {
-        // TODO: Shouldn't this be disposed?
         public static Handler UIThreadHandler { get; } = new Handler(Looper.MainLooper);
     }
 }
