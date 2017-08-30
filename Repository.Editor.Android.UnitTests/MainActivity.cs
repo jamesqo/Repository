f0785473a242c690f0a951db514d502df50799ef
James Ko <jamesqko@gmail.com>
2017-08-30 01:55:50 -0400

    Add unit tests for R.E.A

---
+++ Repository.Editor.Android.UnitTests/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace Repository.Editor.Android.UnitTests
+{
+    [Activity(Label = "Repository.Editor.Android.UnitTests", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            AddTest(Assembly.GetExecutingAssembly());
+
+            base.OnCreate(bundle);
+        }
+    }
+}
+
