563d6045b47cddcf2a9754bfb6c77bd1e9f4ca11
James Ko <jamesqko@gmail.com>
2017-09-02 01:55:00 -0400

    [WIP] Add test to ensure TextColorer does not mess up subsequent colorings when a deletion is made before the color cursor during highlighting

--- Repository.Editor.Android.UnitTests/Highlighting/TextColorerTests.cs
+++ Repository.Editor.Android.UnitTests/Highlighting/TextColorerTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Repository.Editor.Android.Highlighting;
 using Repository.Editor.Android.UnitTests.TestInternal.Collections;
 using Repository.Editor.Android.UnitTests.TestInternal.Editor.Highlighting;
+using Repository.Editor.Android.UnitTests.TestInternal.JavaInterop;
 using Repository.Editor.Android.UnitTests.TestInternal.Threading;
 using Repository.Editor.Highlighting;
 using static Repository.Editor.Highlighting.SyntaxKind;
@@ -14,14 +16,7 @@ namespace Repository.Editor.Android.UnitTests.Highlighting
     [TestFixture]
     public class TextColorerTests
     {
-        [Test]
-        public async void Flushing(
-            [Range(1, 6)] int numberOfFlushes,
-            [Range(1, 6)] int flushSize)
-        {
-            async Task RunTest()
-            {
-                var sourceText = @"
+        private static readonly string JavaSourceCode1 = @"
 package com.mycompany;
 
 class C {
@@ -30,54 +25,97 @@ class C {
         System.out.println(""Scary to see Java in the middle of C# code, isn't it?"");
     }
 }";
-                var colorer = CreateTextColorer(sourceText, out var yielder);
+
+        private static readonly IEnumerable<SyntaxAssignment> JavaSourceCode1Assignments = new SyntaxAssignment[]
+        {
+            ("package", Keyword),
+            ("com", Identifier),
+            (".", Plaintext),
+            ("mycompany", Identifier),
+            (";", Plaintext),
+            ("class", Keyword),
+            ("C", TypeDeclaration),
+            ("{", Plaintext),
+            ("void", Keyword),
+            ("m", MethodDeclaration),
+            ("(", Plaintext),
+            (")", Plaintext),
+            ("{", Plaintext),
+            ("System", Identifier),
+            (".", Plaintext),
+            ("out", Identifier),
+            (".", Plaintext),
+            ("println", MethodIdentifier),
+            ("(", Plaintext),
+            (@"""Hello, world!""", StringLiteral),
+            (")", Plaintext),
+            (";", Plaintext),
+            ("System", Identifier),
+            (".", Plaintext),
+            ("out", Identifier),
+            (".", Plaintext),
+            ("println", MethodIdentifier),
+            ("(", Plaintext),
+            (@"""Scary to see Java in the middle of C# code, isn't it?""", StringLiteral),
+            (")", Plaintext),
+            (";", Plaintext),
+            ("}", Plaintext),
+            ("}", Plaintext)
+        };
+
+        [Test]
+        public async void DeletionBeforeColorCursor_DoesNotAffectPendingColorings()
+        {
+            async Task RunTest()
+            {
+                var sourceCode = JavaSourceCode1;
+                var colorer = CreateTextColorer(sourceCode, out var yielder);
+                SetCallback(yielder, colorer, 0, Callback1);
+
+                using (colorer.FlushEveryToken())
+                {
+                    await Highlighter.Java.Highlight(sourceCode, colorer);
+                }
+
+                AfterHighlight(colorer);
+            }
+
+            void Callback1(CallbackRunnerYielder yielder, TextColorer colorer)
+            {
+                var cursor = colorer.Text.GetCursor(0);
+                cursor.SkipWhitespace().Delete("package");
+            }
+
+            void AfterHighlight(TextColorer colorer)
+            {
+                var expected = JavaSourceCode1Assignments.Skip(1);
+                var actual = colorer.GetSyntaxAssignments().RemoveWhitespaceTokens();
+                Assert.AreEqual(expected, actual);
+            }
+
+            await RunTest();
+        }
+
+        [Test]
+        public async void Flushing(
+            [Range(1, 6)] int numberOfFlushes,
+            [Range(1, 6)] int flushSize)
+        {
+            async Task RunTest()
+            {
+                var sourceCode = JavaSourceCode1;
+                var colorer = CreateTextColorer(sourceCode, out var yielder);
                 SetCallback(yielder, colorer, numberOfFlushes - 1, Callback1);
 
                 using (colorer.Setup(flushSize))
                 {
-                    await Highlighter.Java.Highlight(sourceText, colorer);
+                    await Highlighter.Java.Highlight(sourceCode, colorer);
                 }
             }
 
             void Callback1(CallbackRunnerYielder yielder, TextColorer colorer)
             {
-                var expected = new SyntaxAssignment[]
-                {
-                    ("package", Keyword),
-                    ("com", Identifier),
-                    (".", Plaintext),
-                    ("mycompany", Identifier),
-                    (";", Plaintext),
-                    ("class", Keyword),
-                    ("C", TypeDeclaration),
-                    ("{", Plaintext),
-                    ("void", Keyword),
-                    ("m", MethodDeclaration),
-                    ("(", Plaintext),
-                    (")", Plaintext),
-                    ("{", Plaintext),
-                    ("System", Identifier),
-                    (".", Plaintext),
-                    ("out", Identifier),
-                    (".", Plaintext),
-                    ("println", MethodIdentifier),
-                    ("(", Plaintext),
-                    (@"""Hello, world!""", StringLiteral),
-                    (")", Plaintext),
-                    (";", Plaintext),
-                    ("System", Identifier),
-                    (".", Plaintext),
-                    ("out", Identifier),
-                    (".", Plaintext),
-                    ("println", MethodIdentifier),
-                    ("(", Plaintext),
-                    (@"""Scary to see Java in the middle of C# code, isn't it?""", StringLiteral),
-                    (")", Plaintext),
-                    (";", Plaintext),
-                    ("}", Plaintext),
-                    ("}", Plaintext)
-                };
-
+                var expected = JavaSourceCode1Assignments;
                 int maxTokenCount = numberOfFlushes * flushSize;
                 var actual = colorer.GetSyntaxAssignments().ToArray();
 
