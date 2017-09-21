11d2e1a11d081b7f59bb2493739b4a75ad24cc58
James Ko <jamesqko@gmail.com>
2017-09-21 14:18:55 -0400

    Refactor some language-neutral ANTLR visitor code into a separate class (#187)
    * Refactor some language-neutral ANTLR visitor code into a separate class
    
    * [Polish]

---
+++ Repository.Editor/Internal/Common/Highlighting/AntlrVisitorCore.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Repository.Common.Validation;
+using Repository.Editor.Highlighting;
+
+namespace Repository.Editor.Internal.Common.Highlighting
+{
+    internal class AntlrVisitorCore
+    {
+        private readonly IParseTreeVisitor<Task> _visitor;
+        private readonly ITextColorer _colorer;
+        private readonly IAntlrSyntaxProvider _syntaxProvider;
+        private readonly CommonTokenStream _tokenStream;
+        private readonly CancellationToken _cancellationToken;
+
+        private int _tokenIndex;
+        private ParserRuleContext _lastAncestor;
+        private ReadOnlyList<SyntaxReplacement> _replacements;
+
+        public AntlrVisitorCore(
+            IParseTreeVisitor<Task> visitor,
+            ParserRuleContext tree,
+            ITextColorer colorer,
+            IAntlrSyntaxProvider syntaxProvider,
+            CommonTokenStream tokenStream,
+            CancellationToken cancellationToken)
+        {
+            Verify.NotNull(visitor, nameof(visitor));
+            Verify.NotNull(tree, nameof(tree));
+            Verify.NotNull(colorer, nameof(colorer));
+            Verify.NotNull(syntaxProvider, nameof(syntaxProvider));
+            Verify.NotNull(tokenStream, nameof(tokenStream));
+
+            _visitor = visitor;
+            _colorer = colorer;
+            _syntaxProvider = syntaxProvider;
+            _tokenStream = tokenStream;
+            _cancellationToken = cancellationToken;
+            _lastAncestor = tree;
+            _replacements = ReadOnlyList<SyntaxReplacement>.Empty;
+        }
+
+        // Even if AbstractParseTreeVisitor.AggregateResult() is overridden, the default implementation
+        // of AbstractParseTreeVisitor.VisitChildren() only starts Task objects, it doesn't await them.
+        // It's necessary to override the method in each visitor to await the child tasks.
+        public async Task VisitChildren(IRuleNode node)
+        {
+            int childCount = node.ChildCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                var child = node.GetChild(i);
+                await child.Accept(_visitor);
+            }
+        }
+
+        public Task VisitChildren(ParserRuleContext context, SyntaxReplacement additionalReplacement)
+        {
+            var newReplacements = GetApplicableReplacements(context);
+            newReplacements.Add(additionalReplacement);
+            return VisitChildrenWithReplacements(context, newReplacements.AsReadOnlyList());
+        }
+
+        public Task VisitChildren(ParserRuleContext context, ImmutableArray<SyntaxReplacement> additionalReplacements)
+        {
+            var newReplacements = GetApplicableReplacements(context);
+            newReplacements.AddRange(additionalReplacements);
+            return VisitChildrenWithReplacements(context, newReplacements.AsReadOnlyList());
+        }
+
+        public Task VisitErrorNode(IErrorNode node)
+            => node.Symbol.IsInvalid() ? Task.CompletedTask : VisitTerminal(node);
+
+        public Task VisitTerminal(ITerminalNode node)
+        {
+            var replacementKind = FindTerminalReplacement(node).Kind;
+            return Advance(node, replacementKind);
+        }
+
+        private async Task Advance(IToken token, SyntaxKind kind)
+        {
+            await Approach(token);
+            await Surpass(token, kind);
+        }
+
+        private Task Advance(ITerminalNode node, SyntaxKind replacementKind)
+        {
+            var token = node.Symbol;
+            var kind = _syntaxProvider.SuggestKind(token).TryReplace(replacementKind);
+            return Advance(token, kind);
+        }
+
+        private async Task Approach(IToken token)
+        {
+            int start = _tokenIndex;
+            int end = token.TokenIndex;
+
+            for (int i = start; i < end; i++)
+            {
+                await Surpass(_tokenStream.Get(i));
+            }
+        }
+
+        private SyntaxReplacement FindTerminalReplacement(ITerminalNode node)
+        {
+            var nodePath = NodePath.GetRelativePath(_lastAncestor, node);
+            // The last replacements are the ones that were added most recently, by closer ancestors. Give precedence to those.
+            for (int i = _replacements.Count - 1; i >= 0; i--)
+            {
+                var replacement = _replacements[i];
+                if (nodePath.Equals(replacement.Path))
+                {
+                    return replacement;
+                }
+            }
+
+            return SyntaxReplacement.None;
+        }
+
+        private List<SyntaxReplacement> GetApplicableReplacements(IParseTree tree)
+        {
+            var list = new List<SyntaxReplacement>();
+            var treePath = NodePath.GetRelativePath(_lastAncestor, tree);
+
+            foreach (var replacement in _replacements)
+            {
+                var path = replacement.Path;
+                if (path.TryChangeRoot(treePath, out var newPath))
+                {
+                    list.Add(replacement.WithPath(newPath));
+                }
+            }
+
+            return list;
+        }
+
+        private Task Surpass(IToken token)
+            => Surpass(token, _syntaxProvider.GetKind(token));
+
+        private Task Surpass(IToken token, SyntaxKind kind)
+        {
+            Verify.ValidState(_tokenIndex == token.TokenIndex, "We skipped some tokens!");
+
+            if (kind == SyntaxKind.Eof)
+            {
+                return Task.CompletedTask;
+            }
+
+            _tokenIndex++;
+
+            int count = token.Text.Length;
+            return _colorer.Color(kind, count, _cancellationToken);
+        }
+
+        private async Task VisitChildrenWithReplacements(ParserRuleContext context, ReadOnlyList<SyntaxReplacement> replacements)
+        {
+            int childCount = context.ChildCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                var child = context.GetChild(i);
+                await VisitWithReplacements(child, context, replacements);
+            }
+        }
+
+        private async Task VisitWithReplacements(IParseTree child, ParserRuleContext parent, ReadOnlyList<SyntaxReplacement> replacements)
+        {
+            Debug.Assert(child.Parent == parent);
+
+            var originalAncestor = _lastAncestor;
+            var originalReplacements = _replacements;
+
+            _lastAncestor = parent;
+            _replacements = replacements;
+            await child.Accept(_visitor);
+
+            _lastAncestor = originalAncestor;
+            _replacements = originalReplacements;
+        }
+    }
+}
---
+++ Repository.Editor/Internal/Java/Highlighting/JavaSyntaxProvider.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Antlr4.Runtime;
+using Repository.Editor.Highlighting;
+using Repository.Editor.Internal.Common.Highlighting;
+using static Repository.Editor.Internal.Java.Highlighting.JavaParser;
+
+namespace Repository.Editor.Internal.Java.Highlighting
+{
+    internal sealed class JavaSyntaxProvider : IAntlrSyntaxProvider
+    {
+        public static JavaSyntaxProvider Instance { get; } = new JavaSyntaxProvider();
+
+        private JavaSyntaxProvider()
+        {
+        }
+
+        public SyntaxSuggestion SuggestKind(IToken token)
+        {
+            switch (token.Type)
+            {
+                case AntlrConstants.Eof:
+                    return SyntaxKind.Eof;
+                case ABSTRACT:
+                case ASSERT:
+                case BOOLEAN:
+                case BREAK:
+                case BYTE:
+                case CASE:
+                case CATCH:
+                case CHAR:
+                case CLASS:
+                case CONST:
+                case CONTINUE:
+                case DEFAULT:
+                case DO:
+                case DOUBLE:
+                case ELSE:
+                case ENUM:
+                case EXTENDS:
+                case FINAL:
+                case FINALLY:
+                case FLOAT:
+                case FOR:
+                case IF:
+                case GOTO:
+                case IMPLEMENTS:
+                case IMPORT:
+                case INSTANCEOF:
+                case INT:
+                case INTERFACE:
+                case LONG:
+                case NATIVE:
+                case NEW:
+                case PACKAGE:
+                case PRIVATE:
+                case PROTECTED:
+                case PUBLIC:
+                case RETURN:
+                case SHORT:
+                case STATIC:
+                case STRICTFP:
+                case SUPER:
+                case SWITCH:
+                case SYNCHRONIZED:
+                case THIS:
+                case THROW:
+                case THROWS:
+                case TRANSIENT:
+                case TRY:
+                case VOID:
+                case VOLATILE:
+                case WHILE:
+                    return SyntaxKind.Keyword;
+                case IntegerLiteral:
+                case FloatingPointLiteral:
+                    return SyntaxKind.NumericLiteral;
+                case BooleanLiteral:
+                    return SyntaxKind.BooleanLiteral;
+                case CharacterLiteral:
+                case StringLiteral:
+                    return SyntaxKind.StringLiteral;
+                case NullLiteral:
+                    return SyntaxKind.NullLiteral;
+                case LPAREN:
+                case RPAREN:
+                case LBRACE:
+                case RBRACE:
+                case LBRACK:
+                case RBRACK:
+                case SEMI:
+                case COMMA:
+                case DOT:
+                    return SyntaxKind.Plaintext;
+                case ASSIGN:
+                    return SyntaxKind.Operator;
+                case GT:
+                case LT:
+                    // These tokens should not be considered operators in generic types or methods.
+                    return SyntaxSuggestion.Replaceable(SyntaxKind.Operator);
+                case BANG:
+                case TILDE:
+                    return SyntaxKind.Operator;
+                case QUESTION:
+                    // ? is not an operator in '? extends ...' in generics.
+                    return SyntaxSuggestion.Replaceable(SyntaxKind.Operator);
+                case COLON:
+                case EQUAL:
+                case LE:
+                case GE:
+                case NOTEQUAL:
+                case AND:
+                case OR:
+                case INC:
+                case DEC:
+                case ADD:
+                case SUB:
+                case MUL:
+                case DIV:
+                case BITAND:
+                case BITOR:
+                case CARET:
+                case MOD:
+                case ADD_ASSIGN:
+                case SUB_ASSIGN:
+                case MUL_ASSIGN:
+                case DIV_ASSIGN:
+                case AND_ASSIGN:
+                case OR_ASSIGN:
+                case XOR_ASSIGN:
+                case MOD_ASSIGN:
+                case LSHIFT_ASSIGN:
+                case RSHIFT_ASSIGN:
+                case URSHIFT_ASSIGN:
+                    return SyntaxKind.Operator;
+                case Identifier:
+                    return SyntaxSuggestion.Replaceable(SyntaxKind.Identifier);
+                case AT:
+                    return SyntaxKind.Annotation;
+                case ELLIPSIS:
+                    return SyntaxKind.Plaintext;
+                case WS:
+                    return SyntaxKind.Plaintext;
+                case COMMENT:
+                case LINE_COMMENT:
+                    return SyntaxKind.Comment;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
