c8d30d0c2ca0ecb0e604b0828d98cb11ec3570e6
James Ko <jamesqko@gmail.com>
2017-11-25 08:20:43 -0500

    Add option to build AAR in debug

--- build-aar.ps1
+++ build-aar.ps1
@@ -1,20 +1,32 @@
-$aarName = "repository-release.aar"
+[CmdletBinding(PositionalBinding = $false)]
+Param(
+    [switch] $release
+)
+
+if ($release) {
+    $aarSourceName = "repository-release.aar"
+} else {
+    $aarSourceName = "repository-debug.aar"
+}
 $javaProjectRoot = [IO.Path]::Combine($PSScriptRoot, "java")
-$aarSource = [IO.Path]::Combine($javaProjectRoot, "repository", "build", "outputs", "aar", $aarName)
+$aarSource = [IO.Path]::Combine($javaProjectRoot, "repository", "build", "outputs", "aar", $aarSourceName)
 $javaInteropRoot = [IO.Path]::Combine($PSScriptRoot, "Repository.JavaInterop")
-$aarDestination = [IO.Path]::Combine($javaInteropRoot, "Jars")
+$aarDestination = [IO.Path]::Combine($javaInteropRoot, "Jars", "repository.aar")
 
 pushd $javaProjectRoot
 
-.\gradlew assembleRelease
-if (! $?)
-{
+if ($release) {
+    .\gradlew assembleRelease
+} else {
+    .\gradlew assembleDebug
+}
+if (! $?) {
     exit $LastExitCode
 }
 
-New-Item -Force -ItemType Directory -Path $aarDestination | Out-Null
+New-Item -Force -ItemType Directory -Path $(Split-Path $aarDestination) | Out-Null
 cp $aarSource $aarDestination -Force
 $time = Get-Date -Format "hh:mm:ss tt"
-echo "Successfully copied $aarName to $aarDestination at $time."
+echo "Successfully copied $aarSourceName to $aarDestination at $time."
 
 popd
